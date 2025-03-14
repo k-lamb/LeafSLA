f7d7b27a4e0ee3f9cff202033ac6f491f398ae08
k-lamb <54627148+k-lamb@users.noreply.github.com>
2025-03-13 18:35:57 -0700

    Update requirements.sh
    package version information added but not required

--- requirements.sh
+++ requirements.sh
@@ -1,8 +1,8 @@
 
 ### package requirements
-pip install matplotlib numpy pandas # general use libraries
-pip install opencv-python pillow scikit-image # image-related libraries
-pip install ultralytics torch segment_anything tensorflow # machine learning libraries
+pip install matplotlib numpy pandas # general use libraries. versions are: matplotlib=3.10.1 numpy=1.26.4 pandas=2.2.3
+pip install opencv-python pillow scikit-image # image-related libraries. versions are: opencv=4.11.0.86 pillow=11.1.0 scikit-image=0.25.2
+pip install torch segment_anything tensorflow # machine learning libraries. versions are: segment_anything=1.0 tensorflow=2.19.0
 
 # make directory for SAM work
 mkdir ~/Documents/SAM
